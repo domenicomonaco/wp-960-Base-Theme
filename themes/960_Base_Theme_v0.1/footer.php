688eb5d7977b16181532180228417c0813fac376
Domenico <kiuz.4ever@gmail.com>
2009-06-22 13:48:56 +0200

    starting uploading theme Alpha version

---
+++ themes/960_Base_Theme_v0.1/footer.php
@@ -0,0 +1,58 @@
+<?php
+/*   
+Theme Name: 960 Base Theme
+Theme URI: http://960basetheme.kiuz.it
+Description: Wordpress theme based on 960 Grid System
+Author: Domenico Monaco
+Author URI: http://www.kiuz.it
+Version: 0.1
+*/
+?>
+
+
+</div><!-- end #container -->
+
+<div class="clear">&nbsp;</div>
+
+<!-- 
+<div id="siderbar2" class="footer-widget grid_4">
+	<p>&nbsp;</p>
+    <?php	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Sidebar 0') ) : ?>
+	<?php endif; ?>   
+    </div>
+    	<div id="siderbar2" class="footer-widget grid_4">
+    	<p>&nbsp;</p>
+    <?php	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Sidebar 1') ) : ?>
+	<?php endif; ?>   
+    </div>
+    	<div id="siderbar2" class="footer-widget grid_4">
+    	<p>&nbsp;</p>
+    <?php	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Sidebar 2') ) : ?>
+	<?php endif; ?>   
+    </div>
+    
+<div class="clear">&nbsp;</div>
+-->
+
+
+
+
+<div class="grid_16" id="site_info">
+<div class="box">
+<p><b>960 Base Theme</b> &middot; Athor <b>Domenico M.</b> <a href="http://www.kiuz.it">Kiuz</a> | <a href="http://www.kiuz.it">http://www.kiuz.it</a><br />
+<b>Based on</b> Fluid 960 Grid System - Special Thanks to Original 960 Grid System.</p>
+</div>
+</div>
+
+<!-- END footer -->
+
+<div class="clear">&nbsp;</div>
+
+</div><!-- end #wrapper -->
+
+<?php #wp_footer(); ?>
+
+
+
+</body>
+</html>
---
+++ themes/960_Base_Theme_v0.1/functions.php
@@ -0,0 +1,54 @@
+<?php
+/*   
+Theme Name: 960 Base Theme
+Theme URI: http://960basetheme.kiuz.it
+Description: Wordpress theme based on 960 Grid System
+Author: Domenico Monaco
+Author URI: http://www.kiuz.it
+Version: 0.1
+*/
+?>
+
+<?php
+
+if ( function_exists('register_sidebar') )
+	register_sidebar(array(
+		'name' => 'Sidebar 1',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>',
+	));
+	
+	register_sidebar(array(
+		'name' => 'Footer Sidebar 0',
+		'before_widget' => '<div id="%1$s" class="widget-footer %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>',
+	));
+	
+	register_sidebar(array(
+		'name' => 'Footer Sidebar 1',
+		'before_widget' => '<div id="%1$s" class="widget-footer %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>',
+	));
+	
+	register_sidebar(array(
+		'name' => 'Footer Sidebar 2',
+		'before_widget' => '<div id="%1$s" class="widget-footer %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>',
+	));
+	
+	register_sidebar(array(
+		'name' => 'Header Region 0',
+		'before_widget' => '<div id="%1$s" class="widget-header %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>',
+	));
+?>
